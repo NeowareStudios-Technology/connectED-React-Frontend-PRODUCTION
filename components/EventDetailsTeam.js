e7c7db99b393817073c58820da1f1023dd6b0c8d
Karina Mills <ksmills82@gmail.com>
2019-05-04 17:21:18 -0400

    Completed EventDetailsTeam section - added logic to show remaining volunteer slots, and added scrollview and minor styling

--- components/EventDetailsTeam.js
+++ components/EventDetailsTeam.js
@@ -9,10 +9,7 @@ import {
   ImageBackground,
   ActivityIndicator
 } from "react-native";
-import { Button, Card } from "react-native-elements";
-import { Icon } from "expo";
-import moment from "moment";
-import AppData from "../constants/Data";
+
 
 class EventDetailsTeam extends React.Component {
   constructor(props) {
@@ -23,14 +20,50 @@ class EventDetailsTeam extends React.Component {
     let item = this.props.event;
     let privacyLabel = item.privacy === "o" ? "Open" : "Private";
     return (
-      <>
-        <Text>Event Team</Text>
-        <Text>Pending:</Text>
-        {item.pending_attendees.map((attendee, index)=>(
-          <Text key={index}>{attendee}</Text>
-        ))}
-      </>
+      <ScrollView>
+        <Text style={this.styles.title}>Volunteer Team</Text>
+        <Text style={this.styles.subtitle}>Team Capacity:{' '}{item.capacity}</Text>
+        <Text style={this.styles.subtitle}>Remaining Open Slots:{' '}{parseInt(item.capacity)-(parseInt(item.num_pending_attendees)+parseInt(item.num_attendees) )}</Text>
+        
+        {item.pending_attendees ? 
+        <View>
+          <Text style={this.styles.subtitle}>Pending:{' '}{item.num_pending_attendees}</Text>
+
+          {item.pending_attendees.map((attendee, index)=>(
+            <Text style={this.styles.text} key={index}>{ `\u2022  `}{attendee}</Text>
+          ))}
+
+        </View>
+        :null}
+
+        <Text>{`\n`}</Text>
+        {/* There is no item.attendees -- this is in place for when there is a dataset for attendees that are not pending */}
+        {item.attendees ? 
+        <View>
+          <Text style={this.styles.subtitle}>Confirmed:{' '}{item.attendees}</Text>
+          
+          {item.attendees.map((attendee, index)=>(
+            <Text style={this.styles.text} key={index}>{ `\u2022  `}{attendee}</Text>
+          ))}
+        </View>
+        :null}
+
+      </ScrollView>
     );
+  }
+  styles={
+    title: {
+      fontSize: 30,
+      marginBottom: 15
+    },
+    subtitle: {
+      fontSize: 20,
+      marginBottom: 5
+    },
+    text: {
+      fontSize: 16,
+      lineHeight: 24
+    }
   }
 }
 
