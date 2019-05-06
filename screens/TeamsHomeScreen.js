47965cf3a3f02177554b3eae290af4456f1e7310
Karina Mills <ksmills82@gmail.com>
2019-05-06 01:18:34 -0400

    added Teams section to bottom Tab navigation-working

---
+++ screens/TeamsHomeScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    ActivityIndicator,
+    Image,
+    ImageBackground,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Dimensions,
+    Text,
+    TouchableOpacity,
+    View,
+    FlatList
+  } from "react-native";
+
+export default class TeamsScreen extends Component {
+    render() {
+        return (
+            <Text>This is the Teams Screen</Text>
+        );
+    }
+}
