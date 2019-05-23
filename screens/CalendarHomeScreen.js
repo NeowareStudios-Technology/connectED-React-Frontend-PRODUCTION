83debeb53138554d2138fa826ebefe20c310aced
Karina Mills <ksmills82@gmail.com>
2019-05-23 16:53:18 -0400

    fixed issue with flex not allowing flatlist to scroll. Pulling in created Events to Calendar Page

--- screens/CalendarHomeScreen.js
+++ screens/CalendarHomeScreen.js
@@ -15,6 +15,12 @@ import {
   import { Avatar, Button, Divider, ButtonGroup } from "react-native-elements";
   import moment from "moment"
   import { Icon } from "expo";
+  import User from "../components/User";
+  import Sequencer from "../components/Sequencer";
+  import uuidv4 from 'uuid/v4';
+  import Colors from "../constants/Colors";
+
+
   let screenHeight = Dimensions.get('window').height;
 
 
@@ -46,18 +52,236 @@ export default class MyCalendar extends Component {
 
     this.state = {
         markedDates: [],
-        activeTab: 0
+
+        user: null,
+        events: null,
+        userEvents: null, // events created by user
+        pastEvents: null, // past events the user has volunteered for
+        activeTab: 0,
+        loading: true
     };
     this.updateTab = this.updateTab.bind(this)
     }
 
-    componentDidMount() {
-        this.getEventDates(DummyData)
-        // this.getMarkedDates()
+    updateUser = (user) => {
+        this.setState({user: user})
+      }
+
+  // loads any events the user created and sorts by date
+  loadUserEvent = async (eventName, index, callback) => {
+    let token = await User.firebase.getIdToken();
+    if (token) {
+      try {
+        let url =
+          "https://connected-dev-214119.appspot.com/_ah/api/connected/v1/events/" +
+          eventName;
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+          }
+        }).then(response => {
+          if (response.ok) {
+            try {
+              let responseData = JSON.parse(response._bodyText);
+              if (responseData) {
+                let userEvents = []
+                // keep state immutable by using slice to return new array
+                if (this.state.userEvents) {
+                  userEvents = this.state.userEvents.slice();
+                }
+                let event = responseData;
+                event.key = "user-event-" + uuidv4();
+                userEvents.push(event);
+                this.setState(
+                  {
+                    userEvents: userEvents,
+                    loading: false
+                  },
+                  () => {
+                    callback();
+                  }
+                );
+
+              }
+            } catch (error) { }
+          }
+          else {
+            callback();
+          }
+        });
+      } catch (error) { }
     }
-    updateTab (selectedIndex) {
-        this.setState({selectedIndex})
+  };
+
+
+  // Loads and filters registered events to only include past events sorted by date 
+  loadPastEvent = async (eventName, index, callback) => {
+    let token = await User.firebase.getIdToken();
+    if (token) {
+      try {
+        let url =
+          "https://connected-dev-214119.appspot.com/_ah/api/connected/v1/events/" +
+          eventName;
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+          }
+        }).then(response => {
+          if (response.ok) {
+            try {
+              let responseData = JSON.parse(response._bodyText);
+              // check if date is in past
+              let now = moment()
+              let eventDate = moment(responseData.date, "MM/DD/YYYY")
+              if (responseData && now.isAfter(eventDate, 'day')) {
+                let pastEvents = []
+                // keep state immutable by using slice to return new array
+                if (this.state.pastEvents) {
+                  pastEvents = this.state.pastEvents.slice();
+                }
+                let event = responseData;
+                event.key = "past-event-" + uuidv4();
+                pastEvents.push(event);
+                this.setState(
+                  {
+                    pastEvents: pastEvents,
+                    loading: false
+                  },
+                  () => {
+                    callback();
+                  }
+                );
+
+              } else {
+                // event not in the past
+                if (!this.state.pastEvents) {
+                  this.setState({ pastEvents: [] },
+                    () => {
+                      callback();
+                    })
+                } else {
+                  callback()
+                }
+              }
+            } catch (error) { }
+          }
+          else {
+            callback();
+          }
+        });
+      } catch (error) { }
     }
+  };
+  updateUser() {
+    this.loadUser()
+  }
+  loadEvents = () => {
+    // TODO: Don't include duplicate events. only fetch the event once
+    let sequence = new Sequencer();
+    let registeredEvents = this.state.events.registered_events
+    let userEvents = this.state.events.created_events
+    if (registeredEvents && registeredEvents.length > 0) {
+      registeredEvents.map((eventName, index) => {
+        eventName = eventName.replace("_", "/")
+        sequence.promise(() => {
+          this.loadPastEvent(eventName, index, () => {
+            sequence.next();
+          });
+        });
+      });
+    } else {
+      this.setState({ pastEvents: [] })
+    }
+    if (userEvents && userEvents.length > 0) {
+      userEvents.map((eventName, index) => {
+        eventName = eventName.replace("_", "/")
+        sequence.promise(() => {
+          this.loadUserEvent(eventName, index, () => {
+            sequence.next();
+          });
+        });
+      });
+    } else {
+      this.setState({ userEvents: [] })
+    }
+    sequence.next();
+  };
+
+  loadUserOpportunities = async () => {
+    let userEvents = [];
+    let token = await User.firebase.getIdToken();
+    console.log("token", token)
+    if (token) {
+      try {
+        let url =
+          "https://connected-dev-214119.appspot.com/_ah/api/connected/v1/profiles/" +
+          this.state.user.email +
+          "/events";
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+          }
+        }).then(response => {
+          if (response.ok) {
+            try {
+              let events = JSON.parse(response._bodyText);
+              if (typeof events === "object") {
+                this.setState({ events: events }, () => this.loadEvents())
+              } else {
+                this.setState({ loading: false });
+              }
+            } catch (error) { }
+          } else {
+            this.setState({
+              loading: false
+            });
+          }
+        });
+      } catch (error) { }
+    }
+  }
+
+  componentDidMount() {
+    this.loadUser()
+  }
+  // componentDidUpdate(prevState){
+  //   if(prevState.user !== this.props.navigation.getParam('user')) {
+  //     this.loadUser()
+  //   }
+  // }
+  async loadUser() {
+    let user = await User.isLoggedIn();
+    if (user) {
+      this.setState(
+        {
+          user: user
+        },
+        () => {
+          this.loadUserOpportunities();
+        }
+      );
+    }
+    return true;
+  }
+
+
+
+
+
+
+    // componentDidMount() {
+    //     this.getEventDates(DummyData)
+    //     // this.getMarkedDates()
+    // }
+
+
+
     getEventDates(array) {
         let DatesArray = []
         for(var i = 0; i < array.length; i++){
@@ -65,6 +289,7 @@ export default class MyCalendar extends Component {
             this.setState({markedDates: DatesArray})
         }
     }
+
     // getMarkedDates(){
     //     let datesObject={}
     //     for(var i=0; i<this.state.markedDates.length; i++){
@@ -76,7 +301,19 @@ export default class MyCalendar extends Component {
     // }
     _keyExtractor = (item, index) => item.id;
 
+    updateTab = (activeTab) => {
+        this.setState({ activeTab })
+      }
+
     render() {
+        let sortedEvents;
+        if (!this.state.userEvents) {
+            sortedEvents = null
+          } else {
+            sortedEvents = this.state.userEvents.slice().sort((a, b) => new Date(a.date[0]) - new Date(b.date[0]));
+          }
+        console.log(sortedEvents)
+
         const buttons = ["Volunteering", "My Opportunities"];
         return (
 
@@ -109,13 +346,93 @@ export default class MyCalendar extends Component {
                 }}
             />
             </View>
-            <View style={styles.eventsContainer}>
+            <View style={{ marginTop: 8, flex: 1 }}>
+                <ButtonGroup
+                  onPress={this.updateTab}
+                  selectedIndex={this.state.activeTab}
+                  buttons={buttons}
+                  containerStyle={{ height: 42 }}
+                />
+                <View style={styles.eventsContainer}>
+                  {this.state.activeTab === 0 && (
+                      <View style={{flex:1}}>
+                        {sortedEvents ? (               
+                    
+                        <FlatList
+                            data={sortedEvents}
+                            keyExtractor={this._keyExtractor}
+                            renderItem={({item}) => 
+                                <View style={styles.eventListing}>
+                                    <View style={{width: 50, height: 50, backgroundColor: '#275FBC'}}>
+                                    <Image style={{flex: 1}} resizeMode='cover' source={{ uri:  "data:image/png;base64," + item.e_photo}}></Image>
+                                    </View>
+                                    <View style={{
+                                        marginLeft:10,
+                                        paddingLeft:10,
+                                        borderLeftColor: 'gray',
+                                        borderLeftWidth: 2
+                                    }}>
+                                        <Text style={{fontWeight: 'bold', fontSize: 20}}>{item.e_title}</Text>
+                                        <Text>{moment(item.date, "MM/DD/YYYY").format("MMM Do")}</Text>
+                                        <Text>{item.start[0]}</Text>
+                                    </View>
+                                </View>
+                            }
+                        />
+                        
+                    
+                        ) : (
+                            <ActivityIndicator
+                            style={{ marginBottom: 16 }}
+                            size="small"
+                            color="#0d0d0d"
+                            />
+                        )}
+                    </View>
+                  )}
+                  {this.state.activeTab === 1 && (
+                    <>
+                    {sortedEvents ? (
+                        
+                        <FlatList
+                            data={this.state.userEvents}
+                            keyExtractor={this._keyExtractor}
+                            renderItem={({item}) => 
+                                <View style={styles.eventListing}>
+                                    <View style={{width: 50, height: 50, backgroundColor: '#275FBC'}}/>
+                                    <View style={{
+                                        marginLeft:10,
+                                        paddingLeft:10,
+                                        borderLeftColor: 'gray',
+                                        borderLeftWidth: 2
+                                    }}>
+                                        <Text style={{fontWeight: 'bold', fontSize: 20}}>{item.e_title}</Text>
+                                        {/* <Text>{item.date}</Text>
+                                        <Text>{item.time}</Text> */}
+                                    </View>
+                                </View>
+                            }
+                        />
+                    
+                      ) : (
+                          <ActivityIndicator
+                            style={{ marginBottom: 16 }}
+                            size="small"
+                            color="#0d0d0d"
+                          />
+                        )}
+
+
+                    </>
+                  )}
+
+                </View>
+              </View>
+            {/* <View style={styles.eventsContainer}>
                 <Text style={styles.subtitle}>Volunteering</Text>
                 <ScrollView >
                     <FlatList
                         data={DummyData}
-                        
-                        // extraData={this.state}
                         keyExtractor={this._keyExtractor}
                         renderItem={({item}) => 
                             <View style={styles.eventListing}>
@@ -135,7 +452,7 @@ export default class MyCalendar extends Component {
                     />
         
                 </ScrollView>
-            </View>
+            </View> */}
           </View>
     
         );
@@ -143,13 +460,14 @@ export default class MyCalendar extends Component {
 }
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+        flex: 1,
       backgroundColor: "#3788E0"
     },
     eventsContainer: {
-      paddingLeft: 30,
-      paddingRight: 30,
-      paddingTop: 15,
+        flex: 1,
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 10,
     },
     contentContainer: {
       paddingTop: 12
@@ -179,9 +497,9 @@ const styles = StyleSheet.create({
         paddingTop: 10,
         paddingBottom: 10,
         marginBottom: 10,
-        borderColor: '#275FBC',
+        borderColor: 'white',
         borderWidth: 2,
-        borderRadius: 15,
+        borderRadius: 5,
     }
   });
   
